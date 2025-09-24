752f20c22da98f80e4d7e33121bbdc9588f2359b
ajwang-kajwang <61467419+ajwang-kajwang@users.noreply.github.com>
2025-09-24 20:37:56 +0800

    Update config.py

--- Lab_4/config.py
+++ Lab_4/config.py
@@ -1,20 +1,26 @@
-# Import YOLO from ultralytics
-from ultralytics import YOLO
-import time
+# LAB_DIR/config.py
+from pathlib import Path
 
-# Load the YOLOv8 small model (pretrained on COCO)
-model = YOLO("yolov8x.pt")
+LAB_DIR = Path.cwd()  # or Path('/workspace') inside container
+LAB_DIR.mkdir(exist_ok=True, parents=True)
 
-# Measure inference time
-start_time = time.time()
-# Run inference on an example image (URL or local path)
-results = model("a.jpg",
-                device=0,  # Use GPU for inference
-)
-end_time = time.time()
-total_time = end_time - start_time
-print(f"Inference Time Costs: {total_time:.3f} seconds")
+# Model and I/O
+MODEL_PT = '../Lab4/yolov8x.pt'
+IMG_SIZE = 640
+BATCH = 1
+DYNAMIC = False
+HALF = False
 
-# Show and save results
-for r in results:
-    r.save() 
+# Artifacts
+ONNX_PATH = LAB_DIR / 'yolov8x.onnx'
+ENGINE_PATH = LAB_DIR / 'yolov8x_trt_from_onnx.engine'
+ENGINE_PATH_T2T = LAB_DIR / 'yolov8x_trt_from_torch2trt.engine'
+ENGINE_PATH_T2T_FP16 = LAB_DIR / 'yolov8x_trt_from_torch2trt_fp16.engine'
+T2TRT_STATE = LAB_DIR / 'yolov8x_torch2trt.pth'
+T2TRT_STATE_FP16 = LAB_DIR / 'yolov8x_torch2trt_fp16.pth'
+
+# Dataset
+DATA_YAML = '/datasets/coco/coco_mini_val.yaml'
+
+# Camera
+CAMERA_INDEX = 0  # replace with GStreamer string on Jetson if needed
